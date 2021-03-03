2e0a3cf181dc8525a9b0db3db1fbeb7f2935a523
Jean Carlo <realjcwkm@gmail.com>
2021-03-02 21:50:01 -0400

    add profile component and refactoring

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { ExperienceBar } from "../components/ExperienceBar";
-import Head from 'next/head';
+import { Profile } from "../components/Profile";
+
+import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return (
-    <div className='container'>
-        <ExperienceBar/>
+    <div className={styles.container}>
+        <ExperienceBar />
+        <section>
+          <div>
+            <Profile />
+          </div>
+          <div>
+
+          </div>
+        </section>
       </div>
   )
 }
