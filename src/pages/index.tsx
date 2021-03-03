acf7abb8c9346bb9e7726985931a136e8cb73cbc
Jean Carlo <realjcwkm@gmail.com>
2021-03-02 21:58:51 -0400

    add completedChallenges component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { CompletedChallenges } from "../components/CompletedChallenges";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 
@@ -11,6 +12,7 @@ export default function Home() {
         <section>
           <div>
             <Profile />
+            <CompletedChallenges />
           </div>
           <div>
 
